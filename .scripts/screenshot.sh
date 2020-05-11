9f2c88b8391b56b6e045a108bc5b48875d5daafc
zMoooooritz <moritzbiering.mb@gmail.com>
2020-05-11 17:47:26 +0200

    Updated the screenshot mechanic to now also use scrot for selection screenshots

--- .scripts/screenshot.sh
+++ .scripts/screenshot.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
+
 name=$(date +'%Y-%m-%d_%H:%M:%S.png')
 save_path="/home/moritz/Pictures/screenshots/"
 combined=$save_path$name
-scrot -z -q 100 $combined
-notify-send "Screenshot" "Your screenshot has been saved!" --icon=simplescreenrecorder-idle
+file_num=$(ls $save_path | wc -l)
+
+case $1 in
+    "sel") scrot -l style=solid,width=3 -s -z -q 100 $combined ;;
+    *) scrot -z -q 100 $combined ;;
+esac
+
+[ "$file_num" -lt "$(ls $save_path | wc -l)" ] && notify-send "Screenshot" "Your screenshot has been saved!" --icon=simplescreenrecorder-idle
