af7fcd1d287abb492553e64ae3a7b3cf0b526122
zMoooooritz <moritzbiering.mb@gmail.com>
2020-05-28 15:47:13 +0200

    Added cheatsheet images and updated window.sh

--- .scripts/window.sh
+++ .scripts/window.sh
@@ -1,21 +1,34 @@
 #!/bin/bash
 
+id_path="/home/moritz/.local/share/window/id"
+name_path="/home/moritz/.local/share/window/name"
+
 id=$(xprop -root | awk '/_NET_ACTIVE_WINDOW\(WINDOW\)/{print $NF}')
 
-if [[ $id == "0x0" ]]; then
-    echo ""
-    return
-fi
+[[ $id == "0x0" ]] && echo "" && exit 0
+
+! [ -f $id_path ] && echo "id file does not exist" && exit 0
+! [ -f $name_path ] && echo "name file does not exist" && exit 0
+
+# check if the window changed
+read old_id < $id_path
+read old_name < $name_path
+[[ $id == $old_id ]] && echo $old_name && exit 0
+echo $id > $id_path
+
+[ $(xrandr -q | grep -c " connected") -gt 1 ] &&
+    output="$(i3-msg -t get_workspaces | jq '.[] | select(.focused==true).output' | cut -d"\"" -f2): "
+
 
 name=$(xprop -id $id | awk '/_NET_WM_NAME/{$1=$2="";print}' | cut -d'"' -f2)
 class=$(xprop -id $id | awk '/WM_CLASS/' | cut -d'"' -f2)
 class2=$(xprop -id $id | awk '/WM_CLASS/' | cut -d'"' -f4)
 
-declare -a name_apps=("Telegram" "vimiv" "TeamSpeak 3")
-
+declare -a name_apps=("Telegram" "vimiv")
 declare -a class_upper=("discord" "skype" "spotify" "shutter")
-
 declare -a class_full_upper=("gimp" "obs")
+
+# TODO apps to fix
 
 if [[ $id == "xprop: error"* ]]; then
     # empty workspace
@@ -40,8 +53,8 @@ elif [[ $class =~ "libreoffice" ]]; then
     title="$app - $file"
 elif [[ $class == "nemo" ]]; then
     [[ $class == "" ]] && title="Nemo" || title="Nemo - $name"
-elif [[ $class == "gnome-terminal" ]]; then
-    title="Termite"
+elif [[ $class =~ "microsoft teams" ]]; then
+    title="Microsoft Teams"
 elif [[ ${name_apps[@]} =~ "$(echo $name | cut -d' ' -f1)" ]]; then
     # whatever you want to do when arr contains value
     title="$name"
@@ -51,21 +64,11 @@ elif [[ ${class_upper[@]} =~ "$class" ]]; then
 elif [[ ${class_full_upper[@]} =~ "$class" ]]; then
     # whatever you want to do when arr contains value
     title="${class^^}"
-elif [[ $class == *"termite" ]]; then
-    # termite
-    nm=$(echo $name | cut -c 13-)
-    if [[ ${nm:0:1} == "~" || ${nm:0:1} == "/" ]]; then
-        if [[ ${#nm} > 40 ]]; then
-            title="Termite - ${nm:0:1}/.../$(echo $nm | rev | cut -d'/' -f1-2 | rev)"
-        else
-            title="Termite - $nm"
-        fi
-    else
-        title="$name"
-    fi
+elif [[ $class == "st" ]]; then
+    title="st - $name"
 else
     title="$class"
 fi
 
-echo "$title"
-
+echo $output$title > $name_path
+echo $output$title
