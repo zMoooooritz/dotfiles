52edc772e2b58dfe460be1d816b4c678a67b880a
zMoooooritz <moritzbiering.mb@gmail.com>
2020-08-24 21:25:25 +0200

    Restructure of toggle commands to ease operation

--- .scripts/cheatsheet_dmenu.sh
+++ .scripts/cheatsheet_dmenu.sh
@@ -4,4 +4,12 @@ options="$(ls ~/.cheatsheets | sed 's/_cheatsheet.*//g')"
 
 chosen="$(echo -e "$options" | dmenu -b -i -p "Cheatsheet:")"
 
-[ -z $chosen ] || i3-msg exec "feh  --image-bg black -Z -. ~/.cheatsheets/$chosen*"
+size=$(identify -format '%wx%h' "~/.cheatsheets/$chosen*")
+
+#TODO
+# maximale größe auf Bildschirm festlegen
+# bestimmen welche Achse den Rand zuerste erreicht
+# bestimmen der entsprechenden geometry für das sxiv window
+
+[ -z $chosen ] || i3-msg exec "sxiv -b -N CHEATSHEET ~/.cheatsheets/$chosen*"
+# mglw. option "-s f" einfügen
